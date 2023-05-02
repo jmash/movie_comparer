39b9b711294949ca2e216c11dbd54781dd58ea0e
Jared M Ashcraft <jaredmashcraft@gmail.com>
2023-05-02 00:06:23 -0400

    :construction: Added error page to file structure

---
+++ src/Error.tsx
@@ -0,0 +1,19 @@
+﻿import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+
+export default function ErrorPage() {
+    const error = useRouteError();
+    console.error(error);
+    
+    if (isRouteErrorResponse(error)) {
+        return (
+            <div>
+                <h1>An error has been encountered!</h1>
+                <h2>{error.status}</h2>
+                <p>{error.statusText}</p>
+                {error.data?.message && <p>{error.data.message}</p>}
+            </div>
+        );
+    } else {
+        return <div>An unexpected and unknown error has been encountered!</div>
+    }
+}
