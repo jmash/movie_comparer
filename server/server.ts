ceb71c40a7b38806d046585dffdf8cf76b55432a
jmash <jaredmashcraft@gmail.com>
2023-05-02 15:13:17 -0400

    :poop: temp test console log to see type of response object

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-const express = require('express-serve-static-core');
+const express = require('express');
 const app = express();
 const port = process.env.SERVER_PORT || 5000;
 
@@ -7,7 +7,8 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 // route for retrieving movie title
 app.get('/test',
     (req: Express.Request, res: Express.Response) => {
-        express.res.send({express: 'Test route connecting successfully'});
+        console.log(typeof(res));
+        //res.send({express: 'Test route connecting successfully'});
     });
 
 export {}; // included to comply with typescript standards regarding isolated modules
