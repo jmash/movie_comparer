7581ade999740c54c088fd3092fcf65be04f4c81
jmash <jaredmashcraft@gmail.com>
2023-05-02 19:03:48 -0400

    :poop: Attempting to switch to module imports (problems with tsconfig have arisen)

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,13 @@
-const express = require('express');
-const app = express();
+import Express, { Request, Response } from 'express';
+//const express = require('express');
+const app = Express();
 const port = process.env.SERVER_PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // route for retrieving movie title
 app.get('/test',
-    (req: Express.Request, res: Express.Response) => {
+    (req: Request, res:Response) => {
         console.log(typeof(res));
         res.send({express: 'Test route connecting successfully'});
     });
