14c5a70e3752aff270c4a66567adfb5efb43f7ba
Jared M Ashcraft <jaredmashcraft@gmail.com>
2023-05-07 19:52:03 -0400

    :truck: renamed "root.tsx" file to "Root.tsx" to be more consistent

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Root from "./routes/root";
+import Root from "./routes/Root";
 import Error from "./Error";
 import {
     createBrowserRouter,
