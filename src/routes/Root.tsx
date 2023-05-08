c8f67eec2e1e318a2770027d5f2f7712252b8f65
Jared M Ashcraft <jaredmashcraft@gmail.com>
2023-05-08 15:09:59 -0400

    :poop: This code is incorrect but I have to go to work. It will be fixed.

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,14 +1,38 @@
-﻿import React, {useState, useEffect, useRef} from 'react';
+﻿import React, {useState, useEffect} from 'react';
+import axios, {AxiosResponse} from 'axios';
+
+interface Response {
+    titleText: string,
+    releaseYear: number,
+    primaryImage: string,
+    titleType: string
+}
 
 export default function Root() {
     const [movieInput1, setMovieInput1] = useState("");
     const [movieInput2, setMovieInput2] = useState("");
-    const movieInput1Ref = useRef(String);
-    const movieInput2Ref = useRef(String);
+    const [moviePreds1, setMoviePreds1] = useState([]);
+    const [moviePreds2, setMoviePreds2] = useState([]);
     
     useEffect(() => {
        // update predictions 
         console.log(movieInput1);
+        if (movieInput1.length > 0) {
+            const options = {
+                method: 'GET', // TODO: make the url dependent on development/production
+                url: `http://localhost:5000/get-possible-titles/${movieInput1}`,
+                params: {
+                    exact: 'false',
+                    limit: '5'
+                },
+            };
+            const predsRes = axios.request(options);
+            predsRes.then((res: AxiosResponse) => {
+                console.log(res.data);
+            }).catch((err: any) => {
+                console.error(err);
+            });
+        }
     }, [movieInput1]);
     
     useEffect(() => {
@@ -22,9 +46,12 @@ export default function Root() {
                 <h1>Root Route</h1>
                 <form>
                     <h3>Movie Input 1</h3>
-                    <input id="movieInput1" value={movieInput1} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setMovieInput1(movieInput1Ref.current)}}/>
+                    <input list="moviePreds1" id="movieInput1" value={movieInput1} onChange={(e:React.ChangeEvent<HTMLInputElement>) => {setMovieInput1(e.target.value)}}/>
+                    { moviePreds1.map((el:HTMLDivElement) => {
+                        <div></div>
+                    })} 
                     <h3>Movie Input 2</h3>
-                    <input id="movieInput2" value={movieInput2} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setMovieInput2(movieInput2Ref.current)}}/>
+                    <input id="movieInput2" value={movieInput2} onChange={(e:React.ChangeEvent<HTMLInputElement>) => {setMovieInput2(e.target.value)}}/>
                 </form>
             </div>
         </>
