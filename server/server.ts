2fd6fdac0fd36eb3900e57e88f9e8736a8a7b290
Jared M Ashcraft <jaredmashcraft@gmail.com>
2023-05-08 15:10:57 -0400

    :construction: Finally got the private .env variables converting from ts to js correctly

--- server/server.ts
+++ server/server.ts
@@ -1,28 +1,35 @@
 import Express, { Request, Response, NextFunction } from 'express';
 import axios from 'axios';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
 const app = Express();
 const port = process.env.SERVER_PORT || 5000;
 
-const options = {
-    method: 'GET',
-    url: 'https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids',
-    params: {
-        idsList: 'tt0001702,tt0001856,tt0001857'
-    },
-    headers: {
-        'X-RapidAPI-Key': process.env["X-RapidAPI-Key"],
-        'X-RapidAPI-Host': process.env["X-RapidAPI-Host"]
-    }
-};
-
-
+app.use(cors());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+const rapidAPIKey = process.env["X-RapidAPI-Key"];
+const rapidAPIHost = process.env["X-RapidAPI-Host"];
+
 // route for retrieving movie title
-app.get('/test',
+app.get('/get-possible-titles/:title',
     async (req: Request, res:Response, next:NextFunction) => {
+        const options = {
+            method: 'GET',
+            url: `https://moviesdatabase.p.rapidapi.com/titles/search/title/${req.params.title}`,
+            params: {
+                exact: 'false',
+                limit: '5'
+            },
+            headers: {
+                'X-RapidAPI-Key': rapidAPIKey,
+                'X-RapidAPI-Host': rapidAPIHost
+            }
+        };
+        
         try {
+            console.log(req.params);
             const movieRes = await axios.request(options);
             res.status(200).send(movieRes.data);
             console.log(movieRes.data); 
